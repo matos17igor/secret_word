5be9bea145fc11a40855024974e1802406e77696
matos17igor <igorm17_pvp@hotmail.com>
2025-10-10 14:18:20 -0300

    cria pagina inicial

---
+++ src/components/StartScreen.js
@@ -0,0 +1,13 @@
+import "./StartScreen.css";
+
+const StartScreen = () => {
+  return (
+    <div className="start">
+      <h1>Secret Word</h1>
+      <p>Clique no botao para comecar a jogar</p>
+      <button>Comecar o jogo</button>
+    </div>
+  );
+};
+
+export default StartScreen;
