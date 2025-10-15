3511639c79a96d5a5486266124da2242b79af5fc
matos17igor <igorm17_pvp@hotmail.com>
2025-10-15 15:34:42 -0300

    personaliza css e vincula os components

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,10 +1,49 @@
 import "./Game.css";
 
-const Game = ({ verifyLetter }) => {
+const Game = ({
+  verifyLetter,
+  pickedWord,
+  pickedCategory,
+  letters,
+  guessedLetters,
+  wrongLetters,
+  guesses,
+  score,
+}) => {
   return (
-    <div>
-      <h1>Game</h1>
-      <button onClick={verifyLetter}>Finalizar jogo</button>
+    <div className="game">
+      <p className="points">
+        <span>Pontuacao: {score}</span>
+      </p>
+      <h1>Adivinhe a palavra: </h1>
+      <h3 className="tip">
+        Dica sobre a palavra: <span>{pickedCategory}</span>
+      </h3>
+      <p>Voce ainda tem {guesses} tentativa(s)</p>
+      <div className="wordContainer">
+        {letters.map((letter, i) =>
+          guessedLetters.includes(letter) ? (
+            <span key={i} className="letter">
+              {letter}
+            </span>
+          ) : (
+            <span key={i} className="blankSquare"></span>
+          )
+        )}
+      </div>
+      <div className="letterContainer">
+        <p>Tente advinhar uma letra da palavra: </p>
+        <form>
+          <input type="text" name="letter" maxLength="1" required />
+          <button>Jogar!</button>
+        </form>
+      </div>
+      <div className="wrongLettersContainer">
+        <p>Letras ja utilizadas: </p>
+        {wrongLetters.map((letter, i) => (
+          <span key={i}>{letter}, </span>
+        ))}
+      </div>
     </div>
   );
 };
