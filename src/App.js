8bec6a2c2f8fe6f32bfa0f533f0ffb182e1191d7
matos17igor <igorm17_pvp@hotmail.com>
2025-10-17 13:16:30 -0300

    atualiza logica para escolher categoria e palavra e atualiza css

--- src/App.js
+++ src/App.js
@@ -34,14 +34,19 @@ function App() {
   const pickWordAndCategory = useCallback(() => {
     // pick a random category
     const categories = Object.keys(words);
-    const category =
-      categories[Math.floor(Math.random() * Object.keys(categories).length)];
+    const category = categories[Math.floor(Math.random() * categories.length)];
 
     // pick a random word
-    const word =
-      words[category][
-        Math.floor(Math.random() * Object.keys(categories).length)
-      ];
+    const wordList = words[category] || [];
+    const word = wordList[Math.floor(Math.random() * wordList.length)];
+
+    if (!word) {
+      console.error(
+        "pickWordAndCategory: no word found for category",
+        category
+      );
+      return { word: "", category };
+    }
 
     return { word, category };
   }, [words]);
--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -3,9 +3,10 @@ import "./StartScreen.css";
 const StartScreen = ({ startGame }) => {
   return (
     <div className="start">
-      <h1>Secret Word</h1>
-      <p>Clique no botao para comecar a jogar</p>
-      <button onClick={startGame}>Comecar o jogo</button>
+      <h1>
+        Secret <br /> Word
+      </h1>
+      <button onClick={startGame}>Start</button>
     </div>
   );
 };
